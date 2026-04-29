98d8af0184fa873bfa94d2e9e322ba69bfd829e2
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2026-04-29 11:42:53 +0200

    -

--- cours/test.py
+++
@@ -1,6 +0,0 @@
-from mpmath import mp
-mp.dps = 1000000
-pi_str = mp.nstr(mp.pi, 1000000, strip_zeros=False).replace('3.', '3')
-pos = pi_str.find('230107', 1)  # skip the leading 3
-print(f'Motif trouvé à la position (décimale) : {pos}')
-print(f'Contexte : ...{pi_str[pos-3:pos+10]}...')
