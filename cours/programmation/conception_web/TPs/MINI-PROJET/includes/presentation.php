9f00bfb18e2b1e1eab2c59a70b4bc01179c4facd
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2026-05-04 22:15:09 +0200

    -

--- cours/programmation/conception_web/TPs/MINI-PROJET/includes/presentation.php
+++
@@ -1,5 +0,0 @@
-<div class="presentation-box">
-    <h2>Bienvenue sur RestoMood</h2>
-    <p>Découvrez les meilleures tables de votre ville en fonction de votre humeur du moment. Que vous soyez d'humeur gastronomique, italienne, japonaise ou bistrot, nous avons la recommandation parfaite pour vous.</p>
-    <p>Utilisez notre moteur de recherche pour trouver le restaurant idéal ou explorez nos sélections par cuisine et destination. RestoMood, c'est votre guide pour des expériences culinaires
-</div>
